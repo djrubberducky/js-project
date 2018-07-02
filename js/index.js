09345cbc0c816759ddfcb212a929b2b380272af1
Daria Bilous <daria.bilous@icloud.com>
2018-07-02 16:28:48 +0300

    remove "="

--- js/index.js
+++ js/index.js
@@ -4,20 +4,20 @@ const c = parseFloat (prompt('Вільний член'));
 const result = solveQuadr(a, b, c);
 alert(result);
 
-function solveQuadr(a,b,c){
-    const d = calcD(a, b, c);
+function solveQuadr(A, B, C){
+    const d = calcD(A, B, C);
     let x1, x2;
     if (D > 0) {
         x1 = (-B + Math.sqrt(D)) / (2 * A);
         x2 = (-B - Math.sqrt(D)) / (2 * A);
-        result.innerText = "x1: " + x1 + ";  x2: " + x2;
+        return "x1: " + x1 + ";  x2: " + x2;
     } else if (D < 0){
         x = -B / (2 * A);
         return = "x1: " + x1;
     }
     else if (D < 0) result.innerText = "result: " + D;
 }
-function calcD(a, b, c){
+function calcD(A, B, C){
     return (b*b) - (4 * A* C);
 }
 
